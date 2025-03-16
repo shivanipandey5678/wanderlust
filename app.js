a758f667bd61bc25d5d528d675efd73ddf830814
shivani pandey <shivanipandey0107@gmail.com>
2025-03-16 05:23:29 -0700

    adding routes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing=require("./Model/listings.js")
+const mongo_url = "mongodb://127.0.0.1:27017/mernproject";
+const path=require("path");
 
-const mongo_url = "mongodb://127.0.0.1:27017/WANDERLUST";
-
+const methodOverride = require("method-override");
 async function main(){
     await mongoose.connect(mongo_url);
 };
@@ -16,10 +18,46 @@ main()
     console.log(err);
 })
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method")); 
+
 
 app.get("/health-check",(req,res)=>{
     res.send("everything is great")
 })
+
+//index route
+app.get("/listings",async(req,res)=>{
+   const AllListings=await Listing.find({});
+   res.render("listings/index.ejs",{AllListings})
+})
+
+//create route
+app.get("/listing/new",async(req,res)=>{
+    res.render("listings/create.ejs")
+})
+
+//show route
+app.get("/listing/:id",async(req,res)=>{
+    let {id}=req.params;
+    const info_of_list=await Listing.findById(id);
+    res.render("listings/show.ejs",{info_of_list});
+})
+
+
+
+//delete route
+app.delete("listing/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect("/listings")
+    
+})
+
+
+
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080")
---
+++ init/index.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Listing=require("../Model/listings.js")
+const  sampleListings =require("./data.js")
+const mongo_url = "mongodb://127.0.0.1:27017/mernproject";
+
+async function main(){
+    await mongoose.connect(mongo_url);
+};
+
+
+main()
+.then(()=>{
+    console.log("connected to DB");
+}).catch((err)=>{
+    console.log(err);
+})
+
+async function initDB(){
+   await Listing.deleteMany({});
+   await Listing.insertMany(sampleListings);
+   console.log(sampleListings)
+   console.log("data was initialized");
+}
+initDB();
