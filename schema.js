26c76d4e3053f75ad7f1b7e40577a2e574c64f9d
Shivani Pandey <shivanipandey0107@gmail.com>
2025-03-23 19:54:25 +0530

    Implemented review addition to MongoDB, integrated frontend validation, and validated inputs server-side using Joi.

--- schema.js
+++ schema.js
@@ -13,4 +13,11 @@ const listingSchema = Joi.object({
     }).required()
 });
 
-module.exports = listingSchema;
+const reviewSchema = Joi.object({
+    review: Joi.object({
+        rating:Joi.number().required(),
+        comment:Joi.string().required(),
+    }).required()
+});
+
+module.exports={listingSchema,reviewSchema};
