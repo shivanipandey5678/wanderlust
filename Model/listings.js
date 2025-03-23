cf0a7aac3e1d7589d3bc2e3f1bd285515d3cab2e
Shivani Pandey <shivanipandey0107@gmail.com>
2025-03-23 18:05:31 +0530

    Added review schema and established relation between Review and List schema

--- Model/listings.js
+++ Model/listings.js
@@ -19,6 +19,12 @@ const listingSchema=new mongoose.Schema({
     price: Number,
     location: String,
     country: String,
+    reviews:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"Review"
+        }
+    ]
 })
 
 const Listing=mongoose.model("Listing",listingSchema);
