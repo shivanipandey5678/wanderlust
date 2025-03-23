f3da5a6d661d550b33b641771257e2eb8971acc7
Shivani Pandey <shivanipandey0107@gmail.com>
2025-03-23 11:18:15 +0530

    Enhance server-side error handling and schema validation

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongo_url = "mongodb://127.0.0.1:27017/mernproject";
 const path=require("path");
 const ejsMate=require("ejs-mate");
 const methodOverride = require("method-override");
+const wrapAsync=require("./utlis/wrapAsync.js");
+const ExpressError=require("./utlis/ExpressError.js");
 async function main(){
     await mongoose.connect(mongo_url);
 };
@@ -42,52 +44,82 @@ app.get("/listing/new",async(req,res)=>{
 })
 
 //craete route
-app.post("/listings",async(req,res)=>{
-    let listing=req.body.listing;
-    const newListing = new Listing(listing);
-    console.log(newListing)
-    await newListing.save();
-    res.redirect("/listings")
-})
+// app.post("/listings",async(req,res,next)=>{
+//     try {
+//         let listing=req.body.listing;
+//         const newListing = new Listing(listing);
+//         // console.log(newListing)
+//         await newListing.save();
+//         res.redirect("/listings");
+//     } catch (error) {
+//         next(err);
+//     }
+    
+// })
+
+app.post("/listings",wrapAsync(async(req,res,next)=>{
+        if(!req.body.listing){
+            throw new ExpressError(400,"Send valid data for listinf")
+        }
+        let listing=req.body.listing;
+        const newListing = new Listing(listing);
+        // console.log(newListing)
+        await newListing.save();
+        res.redirect("/listings");
+   
+    
+}));
 
 //show route
-app.get("/listing/:id",async(req,res)=>{
+app.get("/listing/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let info_of_list=await Listing.findById(id);
     // console.log(info_of_list)
     res.render("listings/show.ejs",{info_of_list});
-})
+}));
 
 
 
 
 
 //delete route
-app.delete("/listing/:id",async(req,res)=>{
+app.delete("/listing/:id",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     await Listing.findByIdAndDelete(id);
     res.redirect("/listings")
     
-})
+}));
 
 //navigate to edit page route
-app.get("/listing/:id/editpage",async(req,res)=>{
+app.get("/listing/:id/editpage",wrapAsync(async(req,res)=>{
     let {id}=req.params;
     let searchList= await Listing.findById(id);
     console.log(searchList)
     res.render("listings/editpage.ejs",{searchList})
-})
+}));
 
 //update route
-app.put("/listing/:id",async(req,res)=>{
+app.put("/listing/:id",wrapAsync(async(req,res)=>{
+    if(!req.body.listing){
+        throw new ExpressError(400,"Send valid data for listinf")
+    }
    let {id}=req.params;
    let updatedList=await Listing.findByIdAndUpdate(id, { ...req.body.listing }, { new: true });
    console.log(updatedList)
    res.redirect(`/listing/${id}`)
 
+}));
+
+app.all("*",(req,res,next)=>{
+   next(new ExpressError(404,"Page Not Found"));
 })
 
-
+app.use((err,req,res,next)=>{
+    let {statusCode=500,message="Something went wrong!"}=err;
+    res.render("error.ejs",{statusCode,message})
+    // res.status(statusCode).send(message)
+    // res.send("something went wrong")
+})
 
 app.listen(8080,()=>{
     console.log("server is listening to port 8080")
