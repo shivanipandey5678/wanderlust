2c7f7ec012555c7c6f34808d893c0702cc31943c
Shivani Pandey <shivanipandey0107@gmail.com>
2025-03-23 21:33:47 +0530

    Implemented cascade delete: Removing a review now deletes it from both the Review schema and Listing's reviews array.

--- Model/listings.js
+++ Model/listings.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const Review =require("./reviews.js");
 const listingSchema=new mongoose.Schema({
     title:{
         type:String,
@@ -25,6 +26,13 @@ const listingSchema=new mongoose.Schema({
             ref:"Review"
         }
     ]
+})
+
+listingSchema.post("findOneAndDelete",async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in:listing.reviews}});
+    }
+    
 })
 
 const Listing=mongoose.model("Listing",listingSchema);
