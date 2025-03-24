679b57735d795719f4f07929d1e9b859b60b5b78
Shivani Pandey <shivanipandey0107@gmail.com>
2025-03-25 03:18:14 +0530

    Refactored routes using Express Router for modularity

---
+++ Routes/reviews.js
@@ -0,0 +1,48 @@
+const express=require("express");
+const router=express.Router({mergeParams:true});
+const wrapAsync=require("../utlis/wrapAsync");
+const ExpressError=require("../utlis/ExpressError");
+const Listing=require("../Model/listings");
+const Review=require("../Model/reviews");
+const methodOverride = require("method-override");
+const {listingSchema,reviewSchema}=require("../schema.js");
+
+const validateReview=((req,res,next)=>{
+    let {error}=reviewSchema.validate(req.body);
+    if(error){
+        let errMsg=error.details.map((el)=>el.message.join(","));
+        throw new ExpressError(400,errMsg);
+    }
+    else{
+       next()
+    }
+})
+
+
+//review
+ //Post Review
+ router.post("/",validateReview,wrapAsync(async(req,res)=>{
+     let listing =await Listing.findById(req.params.id);
+     let newReview=new Review(req.body.review);
+     listing.reviews.push(newReview);
+ 
+     await newReview.save();
+     await listing.save();
+     // console.log("New Review Saved");
+     // res.send("New Review Saved")
+     res.redirect(`/listing/${req.params.id}`)
+ }));
+
+
+ 
+ //review 
+ //Delete Review
+ router.delete("/:reviewId",wrapAsync(async(req,res)=>{
+    let {id,reviewId}=req.params;
+    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
+    await Review.findByIdAndDelete(reviewId);
+   
+    res.redirect(`/listing/${req.params.id}`)
+}))
+
+module.exports=router;
