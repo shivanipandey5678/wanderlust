b82f0c3f59a05e9e3e45756b3e8f3602ed19f6d7
Shivani Pandey <shivanipandey0107@gmail.com>
2025-03-26 14:46:11 +0530

    feat: add signup route and user form rendering

--- Model/users.js
+++ Model/users.js
@@ -1,13 +1,16 @@
-const mongoose=require("mongoose");
-const passportLocalMongoose=require("passport-local-mongoose");
-const userSchema= new mongoose.Schema({
-    
-    email:{
-        type:String,
-        required,
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true, // Fixed this line
     }
-   
 });
+
+// Add Passport-Local Mongoose plugin for authentication
 userSchema.plugin(passportLocalMongoose);
-let User=mongoose.model("User",userSchema);
-module.exports=User;
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
